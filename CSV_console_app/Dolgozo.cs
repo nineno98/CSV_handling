46dc0ae61aaadf2890766457b987ffe4c634c52f
nineno98 <2023450@B001-15>
2024-04-30 17:09:35 +0200

    beolvasas működik, kiíratás is

---
+++ CSV_console_app/Dolgozo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSV_console_app
+{
+    //"nev","neme","reszleg","belepesev","ber"
+
+    
+    class Dolgozo
+    {
+        private string nev_;
+        private string neme_;
+        private string reszle_;
+        private int belepesev_;
+        private int ber_;
+
+        public Dolgozo(string nev, string neme, string reszle, int belepesev, int ber)
+        {
+            this.nev_ = nev;
+            this.neme_ = neme;
+            this.reszle_ = reszle;
+            this.belepesev_ = belepesev;
+            this.ber_ = ber;
+        }
+
+        public string Nev { get => nev_; set => nev_ = value; }
+        public string Neme { get => neme_; set => neme_ = value; }
+        public string Reszle { get => reszle_; set => reszle_ = value; }
+        public int Belepesev { get => belepesev_; set => belepesev_ = value; }
+        public int Ber { get => ber_; set => ber_ = value; }
+
+        public override string ToString()
+        {
+            return nev_+", "+neme_+", "+reszle_+", "+belepesev_+", "+ber_;
+        }
+    }
+}
--- CSV_console_app/Program.cs
+++ CSV_console_app/Program.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace CSV_console_app
 {
     class Program
     {
+        static List<Dolgozo> dolgozok = new List<Dolgozo>();
         static void Main(string[] args)
         {
+            
+            beolvas();
+            
 
+            foreach (Dolgozo item in dolgozok)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Program vége");
+            Console.ReadKey();
+        }
+
+        private static void beolvas()
+        {
+            using (StreamReader sr = new StreamReader("dolgozok.csv"))
+            {
+                sr.ReadLine(); //első sor átugrása
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Replace('"',' ').Split(',');
+                    Dolgozo dolgozo = new Dolgozo(line[0].Trim(), line[1].Trim(), line[2].Trim(), int.Parse(line[3].Trim()), int.Parse(line[4].Trim()));
+                    dolgozok.Add(dolgozo);
+                }
+            }
         }
     }
 }
